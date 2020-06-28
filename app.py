72940a85ddf069336bff9650aa94c61ae1193fb7
Azddine <elmoumny.azddine@gmail.com>
2020-06-28 17:09:12 +0100

    add methods get and post

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,url_for
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',method=['get','post'])
 def index():
    return render_template('public/index.html')
 if __name__ == '__main__':
