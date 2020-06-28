289c691e0bd86e564654d92d4741753f5ffa6dc8
Azddine <elmoumny.azddine@gmail.com>
2020-06-28 23:23:25 +0100

    fix errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask ,render_template,url_for
 app = Flask(__name__)
-@app.route('/',method=['get','post'])
+@app.route('/')
 def index():
    return render_template('public/index.html')
 if __name__ == '__main__':
