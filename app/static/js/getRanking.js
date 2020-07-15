ae26faaa802498a98ce93581e1f0b54514515b3f
Mohammed <simozakkar@gmail.com>
2020-07-15 13:32:29 +0100

    add bootstrap table and scale colors to the Graph and fix ..

--- app/static/js/getRanking.js
+++ app/static/js/getRanking.js
@@ -16,10 +16,10 @@ const objtChart = (xlab,ylab,typechart) =>{
 				};
 	return obj;
 }
-
 var mychart ;
-$(function() {
+$(function(){
     $('#getR').click(function() {
+        $('#contentRank').show()
         const inf = {}
         inf[1]=document.getElementById('DiGraph').checked
         inf[2]=document.getElementById('weighted').checked
@@ -30,30 +30,33 @@ $(function() {
                 data:JSON.stringify(inf),
                 dataType: "json",
                 success: function(response) {
-                    // console.log(response );
+                    console.log(response)
+                    $table.bootstrapTable('refreshOptions', {
+                        showColumns: true,
+                        search:true,
+                        data: response
+                    })
                     var xs = [];
                     var ys = [];
-                    for (const property in response) {
-					  xs.push(property);
-					  ys.push(response[property]);
+                    for (const property in response["rows"]) {
+                        if (response["rows"][property]["name"]!="source"&&response["rows"][property]["name"]!="target") {
+                           xs.push(response["rows"][property]["name"]);
+                           ys.push(response["rows"][property]["score"]);
+                        }  
 					}
+                    d3.select('#Ranking_Chart').remove();
+                    d3.select('.Ranking').append('canvas').attr('id','Ranking_Chart').attr('height',"120px");
                     var ctx = document.getElementById('Ranking_Chart').getContext('2d');
 			        mychart = new Chart(ctx,objtChart(xs,ys,'bar'));
                     const cookies = document.cookie.split(";").map(d=>d.split("="));
-                    
                     var secretKey = cookies.filter(d=>d[0]=='CNA_AHP_Key');
-                    // console.log(secretKey)
-                    const namefile = "/static/Data/" + secretKey[0][1]+ ".csv";
-                    // console.log(namefile);
-                    graphchart(namefile);
+                    const namefile = "/static/Data/" +secretKey[0][1]+ ".csv";
+                    graphchart(namefile,xs,ys);
                 },
                 error: function(err) {
-                    // console.log("err")
                     console.log(err);                
                 }
             });
         return false;
     });
 });
-
-
